5e9af8e78ba0aa4ce94f9d035114dbbad593d708
suiyu <chaorensuiyu@gmail.com>
2018-06-13 03:47:05 +0000

    修改欢迎页面

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
-
+        <title>Hello Laravel</title>
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
         <style>
             html, body {
                 height: 100%;
             }
-
             body {
                 margin: 0;
                 padding: 0;
@@ -18,18 +15,15 @@
                 font-weight: 100;
                 font-family: 'Lato';
             }
-
             .container {
                 text-align: center;
                 display: table-cell;
                 vertical-align: middle;
             }
-
             .content {
                 text-align: center;
                 display: inline-block;
             }
-
             .title {
                 font-size: 66px;
             }
